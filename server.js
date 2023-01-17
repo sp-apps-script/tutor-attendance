daedda631009f618f5961037fde98a056bc494bf
Jean Cuadros <bikersprop@gmail.com>
2023-01-17 11:53:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,146 @@
 // Config
-const projectId = 'shareproyect'                         //Id del proyecto
-const datasetId = 'vonex_tutors';                       //Nombre del conjunto de datos
-const today = new Date();
-
-//let userEmail = "jcuadros@vonex.edu.pe";
-let userEmail = Session.getActiveUser().getEmail();
+const projectId = 'bigqueryvonex'                         //Id del proyecto
+const datasetId = 'academy';                              //Nombre del conjunto de datos
 
 // Data
 let writeDisposition = 'WRITE_APPEND';
 let has_header = false;
 let schema_bq = 'no_automatic';
+let today = new Date();
+let userEmail = Session.getActiveUser().getEmail();
 
-// Get Registers Alumns
-function getRegistersAlumns() {
+// DOGET
+function doGet() {
+  let htmlOutput = HtmlService.createTemplateFromFile('index');
+  let alumns = getAlumns();
+
+  htmlOutput.registerAlumns = alumns;
+  htmlOutput.email = userEmail;
+  return htmlOutput.evaluate().setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
+
+// DATA FORMS
+function sendform(alumn,communicate,absence,observation,formAction){
+  let type;
+  if(formAction == "conversations"){
+    type = "conversaciones"
+  }else if(formAction == "calls"){
+    type = "llamadas"
+  }
+
+  let headerConversations = ["id_alumn", "tutor_email", "date", "time", "communicate", "absence", "observation", "type"];
+  let tableId = 'alumn_form';
+
+  // Add Row
+  let newRow = headerConversations.map(function (column) {
+    if (column === 'id_alumn') {
+      return alumn;
+    } else if(column === 'tutor_email') {
+      return userEmail;
+    } else if (column === 'date') {
+      return getDate_();
+    } else if (column === 'time') {
+      return getTime_();
+    } else if (column === 'communicate') {
+      return communicate;
+    } else if (column === 'absence') {
+      return absence;
+    } else if (column === 'observation') {
+      return observation;
+    } else if (column === 'type') {
+      return type;
+    }
+  })
+  let range = [[...newRow]];
+  let schema_table = "form";
+
+  // Insert Row
+  upload_to_BigQ_(range, projectId, datasetId, tableId, writeDisposition, has_header, schema_bq, schema_table);
+  Utilities.sleep(2000)
+  return "ok";
+}
+
+// DATA ATTENDANCE QR / DNI
+function senddataone(dni,state,week) {
+  //let dni= x.substr(-8);
+  let headerAttendance = ["id_alumn", "tutor_email", "week", "day", "state", "date", "time"];
+  let tableId = 'alumn_attendance';
+
+  let cantAttendance = getAlumnsAttendance(dni, userEmail, getDate_());
+
+  if (cantAttendance == 0){
+    // Add Row
+    let newRow = headerAttendance.map(function (column) {
+      if (column === 'id_alumn') {
+        return dni;
+      } else if(column === 'tutor_email') {
+        return userEmail;
+      } else if (column === 'week') {
+        return week;
+      } else if (column === 'day') {
+        return getDay_();
+      } else if (column === 'state') {
+        return state;
+      } else if (column === 'date') {
+        return getDate_();
+      } else if (column === 'time') {
+        return getTime_();
+      }
+    });
+    let range = [[...newRow]];
+    let schema_table = "attendance";
+
+    // Insert Row
+    upload_to_BigQ_(range, projectId, datasetId, tableId, writeDisposition, has_header, schema_bq, schema_table);
+
+    //Utilities.sleep(2000)
+    return "ok";
+  } else {
+    return "no";
+  }
+}
+
+// DATA ATTENDANCE CHECKBOX / FILE
+function senddataall(data,type,week) {
+  let headerAttendance = ["id_alumn", "tutor_email", "week", "day", "state", "date", "time"];
+  let tableId = 'alumn_attendance';
+
+  let newRow = [];
+  let range = [];
+  data.forEach((value,i) => {
+    let attendance = "";
+    let dni;
+
+    if(type == "check"){
+      let newValue = value.split(',');
+      if(newValue[1] != "F"){
+        attendance = newValue[1]
+      }
+      dni = newValue[0];
+    }else{
+      attendance = type;
+      dni = value;
+    }
+
+    newRow.push(dni,userEmail,week,getDay_(),attendance,getDate_(),getTime_())
+    range.push(newRow);
+    newRow = [];
+  })
+
+  let schema_table = "attendance";
+
+  // Insert Row
+  upload_to_BigQ_(range, projectId, datasetId, tableId, writeDisposition, has_header, schema_bq, schema_table);
+
+  return range;
+}
+
+// Get Alumns
+function getAlumns() {
+  let tableId = "alumn";
   let request = {
     query: 'SELECT * ' +
-      'FROM `shareproyect.vonex_tutors.alumns` ORDER BY last_name LIMIT 50;',
+      'FROM '+ projectId + '.' + datasetId +'.'+ tableId + ' LIMIT 50;',
     useLegacySql: false
   };
   let queryResults = BigQuery.Jobs.query(request, projectId);
@@ -53,12 +178,51 @@ function getRegistersAlumns() {
   }
 }
 
+// Delete Attendance
+function deleteAttendance() {
+  let tableId = "alumn_attendance";
+  let request = {
+    query: 'DELETE ' +
+      'FROM '+ projectId + '.' + datasetId +'.'+ tableId + ' LIMIT 50;',
+    useLegacySql: false
+  };
+  let queryResults = BigQuery.Jobs.query(request, projectId);
+
+  if (queryResults.totalRows == 0) {
+    let data = [];
+    return data;
+  } else {
+    const jobId = queryResults.jobReference.jobId;
+    let rows = queryResults.rows;
+    while (queryResults.pageToken) {
+      queryResults = BigQuery.Jobs.getQueryResults(projectId, jobId, {
+        pageToken: queryResults.pageToken
+      });
+      rows = rows.concat(queryResults.rows);
+    }
+
+    // Append the results.
+    let data = new Array(rows.length);
+    for (let i = 0; i < rows.length; i++) {
+      const cols = rows[i].f;
+      data[i] = new Array(cols.length);
+      for (let j = 0; j < cols.length; j++) {
+        data[i][j] = cols[j].v;
+      }
+    }
+
+    //return data;
+    Logger.log(data);
+  }
+}
+
 // Get Alumns Attendance
 function getAlumnsAttendance(id_alumn, userEmail, fecha) {
+  let tableId = "alumn_attendance";
   let request = {
     query: 'SELECT * ' +
-      'FROM `shareproyect.vonex_tutors.alumn_attendance`' + 
-      'WHERE id_alumn = '+ id_alumn +' AND tutor_email = "'+ userEmail +'" AND date = '+ fecha +
+      'FROM '+ projectId +'.'+datasetId +'.'+ tableId + ' ' +
+      'WHERE id_alumn = "' + id_alumn + '" AND tutor_email = "'+ userEmail +'" AND date = '+ fecha +
       'LIMIT 100;',
     useLegacySql: false
   };
@@ -92,77 +256,7 @@ function getAlumnsAttendance(id_alumn, userEmail, fecha) {
   }
 }
 
-// GET
-function doGet() {
-  let htmlOutput = HtmlService.createTemplateFromFile('index');
-  let registerAlumns = getRegistersAlumns();
-
-  htmlOutput.registerAlumns = registerAlumns;
-  return htmlOutput.evaluate().setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
-}
-
-// POST
-function doPost(e) {
-  const lock = LockService.getScriptLock()
-  lock.tryLock(5000)
-
-
-  try {
-    let result;
-    let register = [];
-
-    // Conversations
-    if (e.parameter.action == "conversations" || e.parameter.action == "calls") {
-      let type;
-      if(e.parameter.action == "conversations"){
-        type = "conversations"
-      }else if(e.parameter.action == "calls"){
-        type = "calls"
-      }
-
-      let headerConversations = ["id_alumn", "tutor_email", "date", "time", "communicate", "absence", "observation", "type"];
-      let tableId = 'tutor_help';
-
-      // Add Row
-      let newRow = headerConversations.map(function (column) {
-        if (column === 'tutor_email') {
-          return userEmail;
-        } else if (column === 'date') {
-          return getDate();
-        } else if (column === 'time') {
-          return getTime();
-        } else if (column === 'type') {
-          return type;
-        }else {
-          return e.parameter[column];
-        }
-      })
-      let range = [[...newRow]];
-
-      let schema_table = "form";
-
-      result = "success";
-      // Insert Row
-      upload_to_BigQ_(range, projectId, datasetId, tableId, writeDisposition, has_header, schema_bq, schema_table);
-
-    }
-
-    Utilities.sleep(2000)
-
-    return ContentService
-      .createTextOutput(JSON.stringify({ 'result': result, 'register': register  }))
-      .setMimeType(ContentService.MimeType.JSON)
-  } catch (e) {
-    return ContentService
-      .createTextOutput(JSON.stringify({ 'result': 'error', 'error': e }))
-      .setMimeType(ContentService.MimeType.JSON)
-  }
-
-  finally {
-    lock.releaseLock()
-  }
-}
-
+// Upload Bigquery
 function upload_to_BigQ_(range, projectId, datasetId, tableId, writeDisposition, has_header, schema_bq, schema_table) {
   if (typeof writeDisposition == "undefined") {
     writeDisposition = 'WRITE_EMPTY'
@@ -261,7 +355,7 @@ function upload_to_BigQ_(range, projectId, datasetId, tableId, writeDisposition,
             // Enter a schema below:
             schema: {
               fields: [
-                { name: 'id_alumn', type: 'INTEGER' },
+                { name: 'id_alumn', type: 'STRING' },
                 { name: 'tutor_email', type: 'STRING' },
                 { name: 'date', type: 'DATE' },
                 { name: 'time', type: 'TIME' },
@@ -290,7 +384,7 @@ function upload_to_BigQ_(range, projectId, datasetId, tableId, writeDisposition,
               // Enter a schema below:
               schema: {
                 fields: [
-                  { name: 'id_alumn', type: 'INTEGER' },
+                  { name: 'id_alumn', type: 'STRING' },
                   { name: 'tutor_email', type: 'STRING' },
                   { name: 'week', type: 'INTEGER' },
                   { name: 'day', type: 'INTEGER' },
@@ -315,112 +409,17 @@ function upload_to_BigQ_(range, projectId, datasetId, tableId, writeDisposition,
   Logger.log("Records were uploaded")
 }
 
-// FORMS
-function sendform(alumn,communicate,absence,observation,formAction){
-  let type;
-  if(formAction == "conversations"){
-    type = "conversaciones"
-  }else if(formAction == "calls"){
-    type = "llamadas"
-  }
-
-  let headerConversations = ["id_alumn", "tutor_email", "date", "time", "communicate", "absence", "observation", "type"];
-  let tableId = 'tutor_help';
-
-  // Add Row
-  let newRow = headerConversations.map(function (column) {
-    if (column === 'id_alumn') {
-      return alumn;
-    } else if(column === 'tutor_email') {
-      return userEmail;
-    } else if (column === 'date') {
-      return getDate();
-    } else if (column === 'time') {
-      return getTime();
-    } else if (column === 'communicate') {
-      return communicate;
-    } else if (column === 'absence') {
-      return absence;
-    } else if (column === 'observation') {
-      return observation;
-    } else if (column === 'type') {
-      return type;
-    }
-  })
-  let range = [[...newRow]];
-  let schema_table = "form";
-
-  // Insert Row
-  upload_to_BigQ_(range, projectId, datasetId, tableId, writeDisposition, has_header, schema_bq, schema_table);
-  Utilities.sleep(2000)
-  return "ok";
-}
-
-// ATTENDANCE QR
-function sendback(x,y,w) {
-  let dni= x.substr(-8);
-  let headerAttendance = ["id_alumn", "tutor_email", "week", "day", "state", "date", "time"];
-  let tableId = 'alumn_attendance';
-
-  let cantAttendance = getAlumnsAttendance(dni, userEmail, getDate());
-
-  if (cantAttendance == 0){
-    // Add Row
-    let newRow = headerAttendance.map(function (column) {
-      if (column === 'id_alumn') {
-        return dni;
-      } else if(column === 'tutor_email') {
-        return userEmail;
-      } else if (column === 'week') {
-        return w;
-      } else if (column === 'day') {
-        return getDay();
-      } else if (column === 'state') {
-        return y;
-      } else if (column === 'date') {
-        return getDate();
-      } else if (column === 'time') {
-        return getTime();
-      }
-    });
-    let range = [[...newRow]];
-    let schema_table = "attendance";
-
-    // Insert Row
-    upload_to_BigQ_(range, projectId, datasetId, tableId, writeDisposition, has_header, schema_bq, schema_table);
-
-    //Utilities.sleep(2000)
-    return "ok";
-  } else {
-    return "no";
-  }
-}
-
-// ATTENDANCE CHECKBOX
-function receiveAttendance(type,week,alumns) {
-  let headerAttendance = ["id_alumn", "tutor_email", "week", "day", "state", "date", "time"];
-  let tableId = 'alumn_attendance';
-  //let cantAttendance = getAlumnsAttendance(dni, userEmail, getDate());
-
-  let newRow = [];
-  let range = [];
-  alumns.forEach((value,i) => {
-    newRow.push(value,userEmail,week,getDay(),type,getDate(),getTime())
-    range.push(newRow);
-    newRow = [];
-  })
-
-  let schema_table = "attendance";
-
-  // Insert Row
-  upload_to_BigQ_(range, projectId, datasetId, tableId, writeDisposition, has_header, schema_bq, schema_table);
-
-  //Utilities.sleep(2000)
-  return range;
+// Get Date
+function getDate_(){
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  let day = today.getDate();
+  let date = `"${year}-${month}-${day}"`;
+  return date;
 }
 
 // Get Week
-function getWeek(){
+function getWeek_(){
   //let currentdate = new Date();
   let oneJan = new Date(today.getFullYear(),0,1);
   let numberOfDays = Math.floor((today - oneJan) / (24 * 60 * 60 * 1000));
@@ -429,22 +428,13 @@ function getWeek(){
 }
 
 // Get Day
-function getDay(){
+function getDay_(){
   let day = today.getDay();
   return day;
 }
 
-// Get Date
-function getDate(){
-  let year = today.getFullYear();
-  let month = today.getMonth() + 1;
-  let day = today.getDate();
-  let date = `"${year}-${month}-${day}"`;
-  return date;
-}
-
 // Get Time
-function getTime(){
+function getTime_(){
   let hour = today.getHours();
   let minute = today.getMinutes();
   let seconds = today.getSeconds();
@@ -452,11 +442,9 @@ function getTime(){
   return time;
 }
 
+// Testing
 function testing(){
-  let dni = 75242814;
-  Logger.log(getAlumnsAttendance(dni, userEmail, getDate()));
-}
-
-function sendTest(a,b){
-  return a+b;
+  let param = '74753728,F';
+  let id = param.split(',');
+  Logger.log(id[1]);
 }
